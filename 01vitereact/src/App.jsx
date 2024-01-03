cc48ea7dbf53bdc2b38ab960373ff7d405348570
Nitin singla <nitinsingla702@gmail.com>
2024-01-03 18:11:32 +0530

    Just playing with src of both folders

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Chai from './Chai';
 
 function App() {
  
 
   return (
-     <h1>Chai aur react with vite</h1>   
+     <Chai/>   
   )
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {
+ 
+
+    return (
+       <h1>Chai in react with vite</h1>   
+    )
+  }
+  
+  export default Chai
+  
